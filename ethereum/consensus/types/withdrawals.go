0755b4391a502ebd7061ced80fede454591500ed
Koderholic <meetenebelioluchi@gmail.com>
2025-05-09 11:36:23 +0400

    docs(withdrawals): clarify Amount field in PendingPartialWithdrawal struct
    - Added a comment to the Amount field in the PendingPartialWithdrawal struct to specify that it represents the amount to be withdrawn, specified in Gwei.

--- ethereum/consensus/types/withdrawals.go
+++ ethereum/consensus/types/withdrawals.go
@@ -2,6 +2,6 @@ package types
 
 type PendingPartialWithdrawal struct {
 	ValidatorIndex    string `json:"validator_index"`
-	Amount            string `json:"amount"`
+	Amount            string `json:"amount"` // Amount represents the amount to be withdrawn, specified in Gwei.
 	WithdrawableEpoch string `json:"withdrawable_epoch"`
 }
